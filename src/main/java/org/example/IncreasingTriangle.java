e91cb418579153c259ba9579f102375509031c72
Anjana Dep <w.anjanadep@gmail.com>
2026-05-11 07:53:40 +0530

    Added Menu for the user for selection of the shape that wants to be printed

---
+++ src/main/java/org/example/IncreasingTriangle.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class IncreasingTriangle {
+    IncreasingTriangle(int rows){
+    for(int i=0;i<=rows;i++){
+        for(int j=0;j<i;j++){
+            System.out.print("*");
+        }
+        System.out.println("");
+    }
+}}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,30 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     static void main() {
-            HalfDownTriangle new1= new HalfDownTriangle(5);
-            Square square =new Square(5,5);
+        String [] options={"decreasing Triangle","Square","increasing Triangle"};
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("What shape do you want to print your options are :");
+        for(int i=0;i< options.length;i++){
+            System.out.println(i+": "+options[i]);
+        }
+        try {
+            System.out.print("Enter the selection option number :");
+            int SelectedOption = scanner.nextInt();
+            switch (SelectedOption) {
+                case 0:
+                    HalfDownTriangle new1 = new HalfDownTriangle(5);
+                    break;
+                case 1:
+                    Square square = new Square(5, 5);
+                    break;
+                case 2:
+                    IncreasingTriangle increasingTriangle = new IncreasingTriangle(5);
+                    break;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
